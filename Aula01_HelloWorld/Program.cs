d000f76e3c2a7ad5256e9cb8d8e426a8ec933602
Luiz Carlos Costa Rodrigues <luizccr1977@gmail.com>
2021-08-17 21:21:55 -0300

    primeiro teste

---
+++ Aula01_HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite algo: ");
+            Escrever("Resultado: "+Ler());
+        }
+
+        static void Escrever(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        static string Ler()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
